89bf08f5d1185670b91c93c815ac0d89f1e4bce8
Mar Alonso <mar.alonso@udc.es>
2025-02-22 16:07:43 +0100

    Add frontend controllers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
-import {config} from "dotenv";
 
 const fetchProducts = async (imageUrl: string) => {
-
   try {
     const response = await axios.get('http://localhost:3000/products', {
       params: { imageUrl },
@@ -17,81 +17,73 @@ const fetchProducts = async (imageUrl: string) => {
 };
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [apiResponse, setApiResponse] = useState<any[] | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
-  const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target?.files[0]) {
       const file = event.target?.files[0];
-      await handleSearch(file);
+      setSelectedImage(file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
-  const handleSearch = async (file: File) => {
-    setLoading(true);
-    const imageUrl = URL.createObjectURL(file);
+const handleSearch = async () => {
+  if (selectedImage) {
+    let imageUrl = URL.createObjectURL(selectedImage);
+    imageUrl = imageUrl.replace('blob:', ''); // Elimina el prefijo "blob:"
     const response = await fetchProducts(imageUrl);
     setApiResponse(response);
-    setLoading(false);
-  };
-
-  const handleTextSearch = () => {
-    //TODO: Implement text search functionality
-    console.log("Searching for:", searchQuery);
-  };
-
-  const triggerFileInput = () => {
-    document.getElementById("file-upload")?.click();
-  };
+  }
+};
 
   return (
     <div className="App">
-      <div className="search-container">
-        <img src="/src/assets/logo-FitTEX.png" alt="Logo" className="logo" />
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search products..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button onClick={handleTextSearch}>Search</button>
-          <button
-            onClick={triggerFileInput}
-            disabled={loading}
-            className={`image-search-button ${loading ? "loading" : ""}`}
-            title="Search by image"
-          >
-            <img
-              src="/src/assets/image-search-icon.png"
-              alt="Search by image"
-              className="image-search-icon"
-            />
-          </button>
-        </div>
+      <h1>Buscador de Productos por Imagen</h1>
+      <div>
+        <label htmlFor="file-upload" className="custom-file-upload">
+          Seleccionar imagen
+        </label>
         <input
           id="file-upload"
           type="file"
           accept="image/*"
           onChange={handleImageChange}
-          style={{ display: "none" }}
         />
+        <button onClick={handleSearch}>Buscar productos</button>
       </div>
+
+      {/* Preview of the image */}
+      {imagePreview && (
+        <div className="image-preview-container">
+          <img
+            src={imagePreview}
+            alt="Vista previa de la imagen seleccionada"
+            className="image-preview"
+          />
+          <p className="image-confirmation">✅ Imagen cargada correctamente</p>
+        </div>
+      )}
 
       {/* Show response of the API */}
       {apiResponse && (
-        <div className="results-container">
-          <h2>Products found:</h2>
+        <div>
+          <h2>Productos encontrados:</h2>
           <ul>
             {apiResponse.map((product) => (
               <li key={product.id}>
                 <h3>{product.name}</h3>
                 <p>
-                  Price: {product.price.value.current} {product.price.currency}
+                  Precio: {product.price.value.current} {product.price.currency}
                 </p>
                 <a href={product.link} target="_blank" rel="noopener noreferrer">
-                  See product
+                  Ver producto
                 </a>
               </li>
             ))}
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
   authUrl: 'https://auth.inditex.com:443/openam/oauth2/itxid/itxidmp/sandbox/access_token',
-  clientId: '',
-  clientSecret: '',
+  clientId: 'oauth-mkpsbox-oauthyfalzbasscpcjrqydosnbxpro',
+  clientSecret: '_eVlpkw42{4[rh0P',
   grantType: 'client_credentials',
   scope: 'technology.catalog.read',
 };
